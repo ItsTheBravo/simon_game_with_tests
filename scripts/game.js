2808d7f3a9e4eca9dadc7ab7e080c7486f30a8fd
JPM <johnpmcgrath1996@gmail.com>
2023-04-23 14:10:12 +0100

    Add light function and light tests

--- scripts/game.js
+++ scripts/game.js
@@ -9,11 +9,24 @@ function newGame() {
   game.currentGame = [];
   game.playerMoves = [];
   game.score = 0;
-  showScore();
+    showScore();
+    addTurn();
 }
 
 function showScore() {
   document.getElementById("score").innerText = game.score;
 }
 
-module.exports = { game, newGame, showScore };
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    //showTurns();
+}
+
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add("light");
+    setTimeout(() => {
+        document.getElementById(circ).classList.add("light");
+    }, 400);
+}
+module.exports = { game, newGame, showScore, addTurn, lightsOn };
